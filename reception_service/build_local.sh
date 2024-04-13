30b68374fadde83d9f61999191ca7516683b7e84
Andrés Bayuelo <andres.bayuelo@hotmail.com>
2024-04-13 23:45:04 +0000

    Correction local build.

--- reception_service/build_local.sh
+++ reception_service/build_local.sh
@@ -11,7 +11,7 @@ rm -rf venv/
 echo "Creating new venv"
 python3 -m venv venv
 echo "Activating venv"
-source /workspaces/backend_technical_test_mo/venv/bin/activate
+source /workspaces/reception_service/venv/bin/activate
 echo "Installing requirements"
 pip install --upgrade pip
 pip install -r requirements.txt
