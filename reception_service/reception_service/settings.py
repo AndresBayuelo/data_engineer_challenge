3c560431f28e0a52b0b1ebd3e8c78a9aa2c4c194
Andrés Bayuelo <andres.bayuelo@hotmail.com>
2024-04-14 00:20:50 +0000

    Add postgres configuration.

--- reception_service/reception_service/settings.py
+++ reception_service/reception_service/settings.py
@@ -73,11 +73,22 @@ WSGI_APPLICATION = "reception_service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "db",
+        "PORT": 5432,
+        "NAME": "postalcodes_db",
+        "USER": "postalcodes_user",
+        "PASSWORD": "postalcodes_password",
+    },
 }
 
 
@@ -105,7 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+# TIME_ZONE = "UTC"
+TIME_ZONE = "America/Bogota"
 
 USE_I18N = True
 
